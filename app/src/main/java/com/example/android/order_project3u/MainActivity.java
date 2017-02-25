d5679d1f72990a667d74d15aab3c8d08f951b7c4
Delfantz <lorenzodelfante0@gmail.com>
2017-02-26 00:21:56 +0100

    add Checkbox and "optimize" the instance of object in the onCreate

--- app/src/main/java/com/example/android/order_project3u/MainActivity.java
+++ app/src/main/java/com/example/android/order_project3u/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -20,14 +22,30 @@ public class MainActivity extends AppCompatActivity {
         //TextView Topping_title= (TextView) findViewById(R.id.Topping_title);
         //TextView Order_title= (TextView) findViewById(R.id.OrderSummary_title);
         //TextView Quantity_title= (TextView) findViewById(R.id.Quantity_title);
+        QuantityTW= (TextView) findViewById(R.id.Quantity_info);
+        PriceTW= (TextView) findViewById(R.id.Price_info);
+        CreamCB=(CheckBox) findViewById(R.id.Cream_checkbox);
+        CreamTW=(TextView) findViewById(R.id.Cream_yesOrNo_textBox) ;
 
-
+        CreamCB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(CreamCB.isChecked()==true){
+                    CreamTW.setText("with whipped cream");
+                }else{
+                    CreamTW.setText("without whipped cream");
+                }
+            }
+        });
 
 
 
     }
-    Integer quantity=0;
-    Integer priceForOne=5;
+    int quantity=0;
+    int priceForOne=5;
+    TextView QuantityTW,PriceTW,CreamTW;
+    CheckBox CreamCB;
+    //String palla;
     public void subtract(View v){
         if (quantity>0){
             quantity--;
@@ -43,12 +61,16 @@ public class MainActivity extends AppCompatActivity {
        // TextView PriceTW= (TextView) findViewById(R.id.Price_info);
        // Integer PRICE= quantity*priceForOne;
        // PriceTW.setText(NumberFormat.getCurrencyInstance().format(PRICE));
+     /*   int i=1000;
+        while(i>0){
+            palla+="PALLAUOLO";
+            CreamTW.setText(palla);
+            i--;
+        } */
     }
-    public void updateQuantity(Integer quantity){
-        TextView QuantityTW= (TextView) findViewById(R.id.Quantity_info);
-        QuantityTW.setText(quantity.toString());
-        TextView PriceTW= (TextView) findViewById(R.id.Price_info);
-        Integer PRICE= quantity*priceForOne;
+    public void updateQuantity(int quantity){
+        QuantityTW.setText(""+quantity);
+        int PRICE= quantity*priceForOne;
         PriceTW.setText(NumberFormat.getCurrencyInstance().format(PRICE));
     }
 }
