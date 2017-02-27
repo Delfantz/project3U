a6385b9d94cff4f4e20d51c1cfe65abe5b22611f
Delfantz <lorenzodelfante0@gmail.com>
2017-02-27 22:23:54 +0100

    Style the app with a custom theme (yellow based)

--- app/src/main/java/com/example/android/order_project3u/MainActivity.java
+++ app/src/main/java/com/example/android/order_project3u/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.order_project3u;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +12,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 import java.util.Currency;
@@ -17,6 +20,14 @@ import java.util.Currency;
 import static android.R.attr.y;
 
 public class MainActivity extends AppCompatActivity {
+
+    int quantity=1;
+    int priceForOne=5;
+    TextView listTW,PriceTW;
+    CheckBox CreamCB,ChocolateCB;
+    EditText NameET;
+    int PRICE=0,APRICE=0;
+    String cream_list,chocolate_list,name="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +39,13 @@ public class MainActivity extends AppCompatActivity {
         listTW= (TextView) findViewById(R.id.Quantity_info);
         PriceTW= (TextView) findViewById(R.id.Price_info);
         CreamCB=(CheckBox) findViewById(R.id.Cream_checkbox);
-        CreamTW=(TextView) findViewById(R.id.Cream_yesOrNo_textBox) ;
+        //CreamTW=(TextView) findViewById(R.id.Cream_yesOrNo_textBox) ;
         ChocolateCB=(CheckBox) findViewById(R.id.chocolate_checkbox);
         NameET=(EditText) findViewById(R.id.name_editText);
+        PriceTW.setText(NumberFormat.getCurrencyInstance().format(1));
+        cream_list=getString(R.string.without_cream);
+        chocolate_list=getString(R.string.without_chocolate);
+        //Log.v("MainActivity.java",""+name);
 
 
         CreamCB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -39,14 +54,14 @@ public class MainActivity extends AppCompatActivity {
                 if(CreamCB.isChecked()){
                     APRICE++;
                     Log.v("MaiAtivity.java",""+APRICE);
-                    cream_list="with whipped cream";
-                    CreamTW.setText(cream_list + "\n"+ chocolate_list+"\n"+name);
+                    cream_list=getString(R.string.with_cream);
+                    //CreamTW.setText(cream_list + "\n"+ chocolate_list+"\n"+name);
                     updateQuantity(quantity);
                 }else{
                     APRICE--;
                     Log.v("MaiAtivity.java",""+APRICE);
-                    cream_list="without whipped cream";
-                    CreamTW.setText(cream_list + "\n"+ chocolate_list+"\n"+name);
+                    cream_list=getString(R.string.without_cream);
+                    //CreamTW.setText(cream_list + "\n"+ chocolate_list+"\n"+name);
                     updateQuantity(quantity);
 
                 }
@@ -59,15 +74,15 @@ public class MainActivity extends AppCompatActivity {
                     if(ChocolateCB.isChecked()){
                         APRICE+=2;
                         Log.v("MaiAtivity.java",""+APRICE);
-                        chocolate_list="with chocolate";
-                        CreamTW.setText(cream_list + "\n"+ chocolate_list+"\n"+name);
+                        chocolate_list=getString(R.string.with_chocolate);
+                        //CreamTW.setText(cream_list + "\n"+ chocolate_list+"\n"+name);
                         updateQuantity(quantity);
 
                     }else{
                         APRICE-=2;
                         Log.v("MaiAtivity.java",""+APRICE);
-                        chocolate_list="without chocolate";
-                        CreamTW.setText(cream_list + "\n"+ chocolate_list+"\n"+name);
+                        chocolate_list=getString(R.string.without_chocolate);
+                        //CreamTW.setText(cream_list + "\n"+ chocolate_list+"\n"+name);
                         updateQuantity(quantity);
                     }
             }
@@ -77,23 +92,20 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    int quantity=0;
-    int priceForOne=5;
-    TextView listTW,PriceTW,CreamTW;
-    CheckBox CreamCB,ChocolateCB;
-    EditText NameET;
-    int PRICE=0,APRICE=0;
-    String cream_list="without whipped cream",chocolate_list="without chocolate",name="";
+
     public void subtract(View v){
-        if (quantity>0){
+        if (quantity>1){
             quantity--;
             updateQuantity(quantity);
         }
 
     }
     public void addUp(View v){
-        quantity++;
-        updateQuantity(quantity);
+        if(quantity<100){
+            quantity++;
+            updateQuantity(quantity);
+        }
+
     }
     public void order(View v){
        // TextView PriceTW= (TextView) findViewById(R.id.Price_info);
@@ -105,8 +117,27 @@ public class MainActivity extends AppCompatActivity {
             CreamTW.setText(palla);
             i--;
         } */
+
         name=NameET.getText().toString();
-        CreamTW.setText(cream_list + "\n"+ chocolate_list+"\n"+name);
+        Log.v("MainActivity.java",""+name);
+        if(name.matches("")){
+            Toast.makeText(this,getString(R.string.message_fill_name),Toast.LENGTH_SHORT).show();
+        }else{
+
+            String to[]={getString(R.string.bar_mail)};
+            //CreamTW.setText(cream_list + "\n"+ chocolate_list+"\n"+name);
+            Intent intent= new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:"));
+            intent.putExtra(Intent.EXTRA_TEXT,cream_list + "\n"+ chocolate_list+"\n"+name+"\n"+PriceTW.getText().toString()+"\n"+quantity);
+            intent.putExtra(Intent.EXTRA_SUBJECT,getString(R.string.subjectMail,name));
+            intent.putExtra(Intent.EXTRA_EMAIL,to);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+        }
+
+
+
     }
     public void updateQuantity(int quantity){
 
@@ -116,4 +147,5 @@ public class MainActivity extends AppCompatActivity {
 
         PriceTW.setText(NumberFormat.getCurrencyInstance().format(PRICE));
     }
+
 }
